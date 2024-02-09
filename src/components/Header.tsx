835c11ccaca7300ee7b7a9d531f6407ddd510e49
EthanWakeford <ethanwakeford@gmail.com>
2024-02-08 21:58:12 -0600

    pushing updated components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { Icon } from '@iconify/react';
+import menuIcon from '@iconify/icons-mdi/menu';
+import closeIcon from '@iconify/icons-mdi/close';
 
 interface HeaderProps {
   title: string;
@@ -11,10 +12,9 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = ({ title, navs }) => {
   const [isNavOpen, setIsNavOpen] = useState(false);
   const [isScrolled, setIsScrolled] = useState(false);
-  console.log(`Step 1: ${isNavOpen}`);
 
   useEffect(() => {
-    const headerHeight = 500; // Adjust as needed
+    const headerHeight = 200; // Adjust as needed
 
     const handleScroll = () => {
       const offset = window.scrollY;
@@ -30,14 +30,12 @@ const Header: React.FC<HeaderProps> = ({ title, navs }) => {
 
   const toggleNav = () => {
     setIsNavOpen(!isNavOpen);
-    console.log(isNavOpen);
     setIsScrolled(isNavOpen ? false : true);
-    console.log(`IsScrolled: ${isScrolled}`)
   };
 
   return (
     <header
-      className={`fixed top-0 z-40 flex w-screen justify-between px-4 py-2 text-center align-middle shadow-2xl transition-all duration-1000 lg:grid lg:grid-cols-5 lg:px-0 lg:py-8 ${isScrolled ? 'bg-opacity-100' : 'bg-opacity-50 dark:bg-opacity-50'} bg-ltbg1 lg:hover:bg-opacity-100 dark:bg-dkbg1`}
+      className={`fixed top-0 z-40 flex w-screen justify-between px-4 py-2 text-center align-middle shadow-2xl transition-all duration-1000 lg:grid lg:grid-cols-5 lg:px-0 lg:py-2 ${isScrolled ? 'bg-opacity-100' : 'bg-opacity-50 dark:bg-opacity-50'} bg-ltbg1 lg:hover:bg-opacity-100 dark:bg-dkbg1`}
     >
       <div className='items-center justify-center align-middle lg:col-span-1 lg:flex'>
         <h2 className='text-center text-2xl text-black transition-all duration-300 hover:text-accent dark:text-white'>
@@ -55,14 +53,17 @@ const Header: React.FC<HeaderProps> = ({ title, navs }) => {
           </a>
         ))}
       </nav>
-      <div onClick={toggleNav} className='z-50 w-10 cursor-pointer text-2xl text-black transition-transform duration-300 lg:hidden dark:text-white'>
-        {isNavOpen ? <FontAwesomeIcon icon={faXmark} /> : <FontAwesomeIcon icon={faBars} />}
+      <div
+        onClick={toggleNav}
+        className={`z-50 flex w-10 cursor-pointer items-center justify-center text-2xl text-black transition-transform duration-300 lg:hidden dark:text-white ${isNavOpen ? 'rotate-180 opacity-100' : 'rotate-0 opacity-50'}`}
+      >
+        {isNavOpen ? <Icon icon={closeIcon} /> : <Icon icon={menuIcon} />}
       </div>
 
       <div
         className={`absolute left-0 top-full z-50 w-full transform bg-white shadow-md lg:hidden ${isNavOpen ? 'scale-y-100' : 'scale-y-0'} origin-top transition-transform duration-300 ease-in-out`}
       >
-        <nav className='flex flex-col hover:bg-dkbg2 dark:bg-dkbg1'>
+        <nav className='flex flex-col dark:bg-dkbg1'>
           {navs.map((nav, index) => (
             <a
               key={index}
@@ -73,7 +74,7 @@ const Header: React.FC<HeaderProps> = ({ title, navs }) => {
             </a>
           ))}
           <a href='/contact' className=''>
-            <button className='bg-other my-2 transform rounded-xl bg-tertiary px-4 py-2 text-base transition duration-300 ease-in-out hover:scale-110 hover:bg-accent'>
+            <button className='my-2 mb-4 transform rounded-xl bg-tertiary px-4 py-2 text-base transition duration-300 ease-in-out hover:scale-110 hover:bg-accent'>
               CONTACT US
             </button>
           </a>
@@ -81,7 +82,7 @@ const Header: React.FC<HeaderProps> = ({ title, navs }) => {
       </div>
       <div className='hidden lg:col-span-1 lg:block'>
         <a href='/contact' className=''>
-          <button className='bg-other transform rounded-xl bg-tertiary px-4 py-2 text-lg transition duration-300 ease-in-out hover:scale-110 hover:bg-accent'>
+          <button className='transform rounded-xl bg-tertiary px-4 py-2 text-lg transition duration-300 ease-in-out hover:scale-110 hover:bg-accent'>
             CONTACT US
           </button>
         </a>
